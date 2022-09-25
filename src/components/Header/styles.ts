b5d3f7d27ccfebaac78bb5a43b3584427a3f18de
victor160997 <victor.men.dev@gmail.com>
2022-09-25 20:22:49 -0300

    chores/apagando estilo do button no header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -11,20 +11,4 @@ export const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-  button {
-    font-size: 1rem;
-    color: #fff;
-    background: var(--blue-light);
-    border: 0;
-    padding: 0 2rem;
-    border-radius: 0.25rem;
-    height: 3rem;
-
-    transition: filter 0.2s;
-
-    &:hover {
-      filter: brightness(0.9);
-    }
-  }
 `
